18ca0be6d056969a1a8462a7491aa71bd03e0e17
Ricky Cai <ricky.cai89@gmail.com>
2015-02-09 22:41:13 -0500

    Added Comment and to test another commit + push

--- Project0/src/ProjectZero.java
+++ Project0/src/ProjectZero.java
@@ -1,11 +1,15 @@
+/*
+ * Ricky Cai
+ */
 import java.util.Scanner;
 
 public class ProjectZero {
 
 	public static void main(String[] args) {
 		int num1,num2, sum;
-		System.out.println("Enter two numbers:");
+		System.out.println("Enter two numbers: ");
 		System.out.println("The sum of " + num1 + " and " + num2 + " is " + sum);
+		
 		
 
 	}
