d9fc4765b09d6ffa0abf4b7e90686674177c0ca0
Ricky Cai <ricky.cai89@gmail.com>
2015-02-09 22:38:04 -0500

    Testing Commit + Push, along with declaring variables.

--- Project0/src/ProjectZero.java
+++ Project0/src/ProjectZero.java
@@ -3,7 +3,9 @@ import java.util.Scanner;
 public class ProjectZero {
 
 	public static void main(String[] args) {
-		System.out.println("Test");
+		int num1,num2, sum;
+		System.out.println("Enter two numbers:");
+		System.out.println("The sum of " + num1 + " and " + num2 + " is " + sum);
 		
 
 	}
