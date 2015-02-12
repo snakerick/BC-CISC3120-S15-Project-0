734d63bfd732599a94c229815de9ee8cbedac6cf
Ricky Cai <ricky.cai89@gmail.com>
2015-02-11 20:22:45 -0500

    Finished Program and Added Comments accordingly. Readme.MD is next and might do extra credit.

--- Project0/src/ProjectZero.java
+++ Project0/src/ProjectZero.java
@@ -1,17 +1,29 @@
 /*
  * Ricky Cai
+ * CS3120 THURSDAY Evening.
  */
 import java.util.Scanner;
 
 public class ProjectZero {
 
 	public static void main(String[] args) {
+		//Declaring Variables
 		int num1,num2, sum;
-		System.out.println("Enter two numbers: ");
-		System.out.println("The sum of " + num1 + " and " + num2 + " is " + sum);
+		Scanner stdin;
 		
+		//Declaring Scanner to recieve input
+		stdin = new Scanner( System.in );
 		
-
+		//Print Statement for user
+		System.out.print("Enter two numbers: ");
+		
+		//Storing the numbers and doing calculation for sum
+		num1 = stdin.nextInt();
+		num2 = stdin.nextInt();
+		sum = num1 + num2;
+		System.out.print("The sum of " + num1 + " and " + num2 + " is: " + sum);
+		
+	
 	}
 
 }
